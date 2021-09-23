d966383f30f28475a231cc0ddadbb2895e9dfc2b
vtorres96 <victorcdc96@gmail.com>
2021-09-22 21:50:54 -0300

    adicionada aula18 - resolvendo lista de exercicios junto com a turma

--- aula17/routes/index.js
+++ aula17/routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET pagina de contato */
+router.get('/contato', function(req, res, next) {
+  res.render('contato', { title: 'Petshop DH - Contato' });
+});
+
+router.get('/sobre', function(req, res, next){
+  res.render('sobre', { title: 'Petshop DH - Sobre Nós' });
+});
+
 module.exports = router;
