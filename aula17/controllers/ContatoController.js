1cdbe4786df2f0df4651bcae5e695163f903b511
vtorres96 <victorcdc96@gmail.com>
2021-09-29 20:46:14 -0300

    aula19 - recebendo propriedades vindas do preenchimento do formulario pelo usuario no frontend

--- aula17/controllers/ContatoController.js
+++ aula17/controllers/ContatoController.js
@@ -1,5 +1,12 @@
+const contatos = require('../data/contato');
+
 module.exports = {
   cadastrar(req, res, next){
-    res.send('contato funcional')
+    let { nome, email, mensagem } = req.body;
+
+    let id = contatos.length + 1;
+    let user = { id, nome, email, mensagem };
+
+    res.json(user);
   }
 }
---
+++ aula17/data/contato.js
@@ -0,0 +1,10 @@
+let contatos = [
+  {
+    "id": 1,
+    "nome": "Abirosvaldo da Silva",
+    "email": "abira@osvaldo.com.br",
+    "mensagem": "Olá estou entrando em contato devido lorem ...."
+  },
+];
+
+module.exports = contatos;
