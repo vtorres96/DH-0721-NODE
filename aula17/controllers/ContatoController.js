15ced4a3bf14662f994b79baee6736361e2c26ae
vtorres96 <victorcdc96@gmail.com>
2021-09-29 22:01:24 -0300

    aula19 - adicionando registros em json e retornando mensagem para o usuario na tela

--- aula17/controllers/ContatoController.js
+++ aula17/controllers/ContatoController.js
@@ -1,12 +1,34 @@
+const path = require('path');
+const fs = require('fs');
+
 const contatos = require('../data/contato');
 
 module.exports = {
   cadastrar(req, res, next){
     let { nome, email, mensagem } = req.body;
-
+    
+    /* criando objeto para adicionar no array contatos */
     let id = contatos.length + 1;
-    let user = { id, nome, email, mensagem };
+    let contato = { id, nome, email, mensagem };
 
-    res.json(user);
+    /* adicionando objeto dentro do array contatos */
+    contatos.push(contato);
+
+    /* transformando o array contatos em uma string 
+    para sobrescrever o conteudo do data/contatos.js */
+    let conteudoString = JSON.stringify(contatos);
+    
+    /* obtendo o caminho ate o arquvivo data/contato.js 
+    para nao sobrescrever arquivos incorretos */
+    let caminhoArquivo = path.join('data', 'contato.js');
+
+    /* sobrescrever o conteudo do arquivo data/contato.js */
+    fs.writeFileSync(caminhoArquivo, 'module.exports = ');
+    fs.appendFileSync(caminhoArquivo, conteudoString);
+    
+    res.render('contato', {
+      title: 'Petshop DH - Contato',
+      adicionado: true
+    });
   }
 }
--- aula17/data/contato.js
+++ aula17/data/contato.js
@@ -1,10 +1,26 @@
-let contatos = [
-  {
-    "id": 1,
-    "nome": "Abirosvaldo da Silva",
-    "email": "abira@osvaldo.com.br",
-    "mensagem": "Olá estou entrando em contato devido lorem ...."
-  },
-];
-
-module.exports = contatos;
+module.exports = [{
+  "id": 1,
+  "nome": "Abirosvaldo da Silva",
+  "email": "abira@osvaldo.com.br",
+  "mensagem": "Olá"
+}, {
+  "id": 2,
+  "nome": "Victor",
+  "email": "vtorres@digitalhouse.com.br",
+  "mensagem": "Xablau"
+}, {
+  "id": 3,
+  "nome": "Bruno",
+  "email": "bruno@vaicorinthians.com.br",
+  "mensagem": "vai corinthians com mundial"
+}, {
+  "id": 4,
+  "nome": "Vamos testando",
+  "email": "teste@teste.com",
+  "mensagem": "estou testando inserção do quarto registro"
+}, {
+  "id": 5,
+  "nome": "Testinho",
+  "email": "testinho@gmail.com",
+  "mensagem": "testinho 123"
+}]
