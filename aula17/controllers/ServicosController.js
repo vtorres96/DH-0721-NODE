d352388646526b4481ac3d98b37d894390d720b8
vtorres96 <victorcdc96@gmail.com>
2021-10-04 22:04:23 -0300

    aula20: servico de upload de imagem para nosso projeto e armazenando o nome da imagem no arquivo json para listar imagem correta de acordo com cada servico

--- aula17/controllers/ServicosController.js
+++ aula17/controllers/ServicosController.js
@@ -1,4 +1,5 @@
 let servicos = require('../data/servicos');
+const armazenaRegistrosNoJson = require('../utils/armazena-registros-json');
 
 module.exports = {
   index(req, res, next){
@@ -15,6 +16,23 @@ module.exports = {
   },
 
   salvar(req, res, next){
+    let { titulo, descricao, valor } = req.body;
+    let { filename } = req.file;
+
+    /* criando objeto para adicionar no array servicos */
+    let id = servicos.length + 1;
+    let servico = { id, titulo, descricao, valor, imagem: filename };
+
+    /* adicionando objeto dentro do array servicos */
+    servicos.push(servico);
+
+    /* executando metodo para atualizar os registros
+     do arquivo servico.json */
+    armazenaRegistrosNoJson(servicos, 'servicos.js');
     
+    res.render('adicionar-servico', {
+      title: 'Petshop DH - Serviços',
+      adicionado: true
+    });
   }
 }
--- aula17/routes/servicos.js
+++ aula17/routes/servicos.js
@@ -1,12 +1,24 @@
-let express = require('express')
-let router = express.Router();
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
 
 /* importando ServicosController */
-let ServicosController = require('../controllers/ServicosController');
+const ServicosController = require('../controllers/ServicosController');
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: path.resolve(__dirname, '..', 'public', 'uploads'),
+  filename: function(req, file, cb) {
+     cb(null, Date.now() + '-' + file.originalname)
+  }
+});
+
+const upload = multer({ storage: storage });
 
 router.get('/', ServicosController.index);
 
 router.get('/adicionar', ServicosController.cadastrar);
-router.post('/adicionar', ServicosController.salvar);
+router.post('/salvar', upload.single('imagem'), ServicosController.salvar);
 
 module.exports = router;
