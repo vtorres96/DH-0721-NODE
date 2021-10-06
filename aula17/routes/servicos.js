c6e6e73f8965510b0a59fc7fcb2dae6c4129e3f2
vtorres96 <victorcdc96@gmail.com>
2021-10-06 20:11:59 -0300

    exportando a configuracao de upload de arquivos

--- aula17/routes/servicos.js
+++ aula17/routes/servicos.js
@@ -1,18 +1,11 @@
 const express = require('express');
 const multer = require('multer');
-const path = require('path');
+const storage = require('../config/file-upload');
 
 /* importando ServicosController */
 const ServicosController = require('../controllers/ServicosController');
 
 const router = express.Router();
-
-const storage = multer.diskStorage({
-  destination: path.resolve(__dirname, '..', 'public', 'uploads'),
-  filename: function(req, file, cb) {
-     cb(null, Date.now() + '-' + file.originalname)
-  }
-});
 
 const upload = multer({ storage: storage });
 
