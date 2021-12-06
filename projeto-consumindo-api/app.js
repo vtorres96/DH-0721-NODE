ce9e1e4161edf8778e1f38b2455cca67e66a075e
vtorres96 <victorcdc96@gmail.com>
2021-12-06 20:22:18 -0300

    removendo importacoes desnecessarias no app e eliminando uso de sede session no header

--- projeto-consumindo-api/app.js
+++ projeto-consumindo-api/app.js
@@ -3,13 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const multer = require('multer');
-const storage = require('./config/file-upload');
-const session = require('express-session');
 
-const upload = multer({ storage: storage });
-
-var clientesRouter = require('./routes/clientes');
+var indexRouter = require('./routes/index');
 
 var app = express();
 
@@ -17,21 +12,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(session({
-  secret: 'aula28-sequelize',
-  resave: true,
-  saveUninitialized: true
-}));
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(upload.single('imagem'));
-
-app.use('/clientes/', clientesRouter);
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
