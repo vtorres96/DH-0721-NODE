6f1b01c1282628e34b168b5e017baebde60c0cd7
vtorres96 <victorcdc96@gmail.com>
2021-12-06 20:42:41 -0300

    iniciando servidor no proprio arquivo app.js

--- projeto-consumindo-api/app.js
+++ projeto-consumindo-api/app.js
@@ -1,39 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express')
+const cors = require('cors')
 
-var indexRouter = require('./routes/index');
+const routes = require('./routes/index')
 
-var app = express();
+const app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(3333, () => {
+  console.log('Servidor rodando na porta http://localhost:3333')
+})
