eb0cfc42b5e7bcb64ec17fc935d567b93648869d
vtorres96 <victorcdc96@gmail.com>
2021-12-08 20:15:30 -0300

    adicionando parametro limit no metodo obter todos e metodo obterPorId

--- projeto-consumindo-api/controllers/FotoController.js
+++ projeto-consumindo-api/controllers/FotoController.js
@@ -1,9 +1,27 @@
 const api = require('../config/api')
 
 module.exports = {
-  async api(req, res, next) {
+  async obterTodos(req, res, next) {
     try {
-      let { data } = await api.get('/photos')
+      let limit = req.params.limit
+      let { data } = await api.get('/photos', {
+        params: {
+          _limit: limit
+        }
+      })
+
+      return res.status(200).json(data)
+    } catch (error) {
+      return res.status(400).json({
+        message: 'Erro: ' + error
+      })
+    }
+  },
+
+  async obterPorId(req, res, next) {
+    try {
+      let id = req.params.id
+      let { data } = await api.get('/photos/' + id)
 
       return res.status(200).json(data)
     } catch (error) {
--- projeto-consumindo-api/routes/index.js
+++ projeto-consumindo-api/routes/index.js
@@ -3,7 +3,8 @@ const router = Router()
 
 const FotoController = require('../controllers/FotoController')
 
-router.get('/api/fotos', FotoController.api)
+router.get('/api/todas-fotos/:limit', FotoController.obterTodos)
+router.get('/api/fotos/:id', FotoController.obterPorId)
 router.get('/fotos', FotoController.index)
 
 module.exports = router 
