c787a0ca73d1b8cd020390810977a609d4174393
vtorres96 <victorcdc96@gmail.com>
2021-12-08 22:04:48 -0300

    refatorando controller para listar repositorios do usuario quando encontrar usuario

--- projeto-consumindo-api/controllers/GitHubController.js
+++ projeto-consumindo-api/controllers/GitHubController.js
@@ -1,11 +1,24 @@
+const { gitHubApi } = require('../config/api')
+
 module.exports = {
   async index(req, res, next) {
     try {
+      let usuario = req.query.usuario
+      if (usuario) {
+        let { data } = await gitHubApi.get('/' + usuario + '/repos')
+
+        return res.render('listar-repositorios', { 
+          repositorios: data,
+          usuario: usuario
+        })
+      }
+
       return res.render('listar-repositorios', {
         repositorios: [],
         usuario: null
       })
     } catch (error) {
+      console.log(error)
       return res.render('erro', { erro: error })
     }
   }
